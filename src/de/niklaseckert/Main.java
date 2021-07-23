41aa90352ffee7803abb264be28dd2ea26dfac13
NiklasEckert <niklas.eckert.1807@gmail.com>
2021-07-23 17:16:47 +0200

    added: function printHelp()

--- src/de/niklaseckert/Main.java
+++ src/de/niklaseckert/Main.java
@@ -5,4 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    private static void printHelp() {
+        System.out.println("FolderSize Help:");
+        System.out.println("Run command: java -jar folderSize.jar [OPTIONS] path");
+        System.out.println();
+        System.out.println("    --help  Show help");
+        System.out.println("    -d      Set display depth (Default = 3)");
+        System.out.println("    -t      Set Sorting Type (\"PATH\" (Default), \"SIZE\")");
+    }
 }
